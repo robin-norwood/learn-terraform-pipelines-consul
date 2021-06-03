743092dd383afc3700eb03826d22118b0da95a0a
Robin Norwood <robin.norwood@gmail.com>
2021-06-03 08:24:00 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ terraform {
     organization = "infrastructure-pipelines-workshop"
 
     workspaces {
-      name = "john-d-consul"
+      name = "robin-n-consul"
     }
   }
   required_providers {
